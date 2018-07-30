f788ca393b4ace4bc3c39fab35a64de6c6fd2e5c
Louis Glick <mrldglick@gmail.com>
2018-07-30 11:49:18 +0100

    navbar done, added more files in preparation

---
+++ models/audiobook.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+//
+// const albumSchema = new mongoose.Schema({
+//   name: String,
+//   author: { type: String, required: true },
+//   bookCover: { type: String, required: true },
+//   narrator: { type: String, required: true },
+//   publicationYear: Number,
+//   genres: [{type: String}],
+//   length: Number,
+//   comments: [{ name: String, content: String}]
+//   //is time something that can be defined in a number or is it a string. Does not matter right now but would be good to find out.
+// });
---
+++ models/author.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose');
+//
+// const albumSchema = new mongoose.Schema({
+//   author: { type: String, required: true },
+//   authorPic: { type: String, required: true },
+//   age: Number,
+//   genres: [{type: String}],
+//   numberOfPublishedAudiobooks: Number,
+//   // find some way to have number updated itself
+//   audiobooks: [###]
+// });
+//
+// const model = mongoose.model('Album', albumSchema);
+// module.exports = model;
+//
+
+
+
+//change album to audiobook
