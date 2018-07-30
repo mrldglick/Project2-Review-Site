c372dd1c2b780f1326f4facf7548f90b2db4cea4
Louis Glick <mrldglick@gmail.com>
2018-07-30 10:37:03 +0100

    filled consts for app.js file

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-test
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const bodyParser = require('body-parser');
+const { PORT, DB_URI} = require('./config/enviroment');
+const session = require('express-session');
+const User = require('./models/user');
+const flash = require('express-flash');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(DB_URI);
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+
+
+
+app.listen(PORT, () => console.log(`Express is listening on ${PORT}`));
---
+++ config/enviroment.js
@@ -0,0 +1,6 @@
+const DB_URI = 'mongodb://localhost/Project2-Review-Site';
+const PORT = 8000;
+
+module.exports = {
+  DB_URI, PORT
+};
