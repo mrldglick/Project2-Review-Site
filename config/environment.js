e0b69cbc3df330cbda980c4e556f8ec4568cfded
Louis Glick <mrldglick@gmail.com>
2018-08-02 12:10:28 +0100

    updadted package.json for horuku

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
-const DB_URI = 'mongodb://localhost/Project2-Review-Site';
-const PORT = 8000;
+const DB_URI = process.env.MONGODB_URI ||'mongodb://localhost/Project2-Review-Site';
+const PORT = process.env.PORT || 8000;
 
 module.exports = {
   DB_URI, PORT
